db6196656c2e6c237ea19665ee5f548ca2002b30
Chenhao Liu <jameschenhaoliu@gmail.com>
2025-01-31 23:36:30 +0000

    change event to meeting and add number of people to meeting

--- sql/schema.sql
+++ sql/schema.sql
@@ -62,37 +62,38 @@ CREATE TABLE RoomEquipment ( -- many-to-many
 	CONSTRAINT fk_equipment_room FOREIGN KEY (equipment_id) REFERENCES Equipment(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS EventType;
+DROP TABLE IF EXISTS MeetingType;
 
-CREATE TABLE EventType (
+CREATE TABLE MeetingType (
 	id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	name VARCHAR(255) NOT NULL UNIQUE
 ) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS PublicEvent;
+DROP TABLE IF EXISTS Meeting;
 
-CREATE TABLE PublicEvent (
+CREATE TABLE Meeting (
 	id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-	public_event_name VARCHAR(255) NOT NULL,
-	public_event_description TEXT,
-	public_event_date DATE NOT NULL,
+	`name` VARCHAR(255) NOT NULL,
+	meeting_description TEXT,
+	meeting_date DATE NOT NULL,
 	room_id INT UNSIGNED NOT NULL,
 	starting_time TIME NOT NULL,
-	ending_time TIME NOT NULL,	
+	ending_time TIME NOT NULL,
+	number_of_people INT UNSIGNED NOT NULL (number_of_people > 0),
 	organizer_id INT UNSIGNED NOT NULL,
-	event_type_id INT UNSIGNED NOT NULL,
-	CONSTRAINT fk_event_room FOREIGN KEY (room_id) REFERENCES Room(id) ON DELETE CASCADE,
-	CONSTRAINT fk_event_organizer FOREIGN KEY (organizer_id) REFERENCES Team(id) ON DELETE CASCADE,
-	CONSTRAINT fk_event_type FOREIGN KEY (event_type_id) REFERENCES EventType(id) ON DELETE CASCADE
+	meeting_type_id INT UNSIGNED NOT NULL,
+	CONSTRAINT fk_meeting_room FOREIGN KEY (room_id) REFERENCES Room(id) ON DELETE CASCADE,
+	CONSTRAINT fk_meeting_organizer FOREIGN KEY (organizer_id) REFERENCES Team(id) ON DELETE CASCADE,
+	CONSTRAINT fk_meeting_type FOREIGN KEY (event_type_id) REFERENCES EventType(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS EventParticipant;
+DROP TABLE IF EXISTS MeetingParticipant;
 
-CREATE TABLE EventParticipant (
-	event_id INT UNSIGNED NOT NULL,
+CREATE TABLE MeetingParticipant (
+	meeting_id INT UNSIGNED NOT NULL,
 	user_id INT UNSIGNED NOT NULL,
-	mandatory BOOLEAN NOT NULL DEFAULT TRUE, -- whether the participant is mandatory at event
-	PRIMARY KEY(event_id, user_id),
-	CONSTRAINT fk_event_participant FOREIGN KEY (event_id) REFERENCES PublicEvent(id) ON DELETE CASCADE,
-	CONSTRAINT fk_participant_event FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE
+	mandatory BOOLEAN NOT NULL DEFAULT TRUE, -- whether the participant is mandatory at meeting
+	PRIMARY KEY(meeting_id, user_id),
+	CONSTRAINT fk_meeting_participant FOREIGN KEY (meeting_id) REFERENCES Meeting(id) ON DELETE CASCADE,
+	CONSTRAINT fk_participant_meeting FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE
 ) ENGINE=InnoDB;
